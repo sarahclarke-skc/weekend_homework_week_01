58266bc16914ed16f48b938235cf6b1e5897469d
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-07-23 21:18:52 +0100

    tests passed. customer can afford pet.

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -66,3 +66,8 @@ def get_customer_pet_count(customer):
 def add_pet_to_customer(customer, pet):
     customer["pets"].append(pet)
 
+def customer_can_afford_pet(customer, pet):
+    if customer["cash"] >= pet["price"]:
+        return True
+    else:
+        return False
